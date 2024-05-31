7f388887b68f64ccfb6a93e7d77a0dc48a46122a
Jacob Heyes <heyesjacob@gmail.com>
2024-05-31 10:39:06 +0100

    tailwind css added

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./output.css" rel="stylesheet">
+    <title>Home</title>
+</head>
+<body>
+    <main className="xl (1280px)">
+    <div className="Btn-blue">
+        <a href ='' name='EnterPortfolio'>Enter Portfolio</a>
+    </div>
+</body>
+</html>
