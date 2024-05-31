abbe571418ebeea9e39ab265f2d06d4faac8b2a7
Jacob Heyes <heyesjacob@gmail.com>
2024-05-31 11:00:51 +0100

    test

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./output.css" rel="stylesheet">
+    <link href="src/output.css" rel="stylesheet">
     <title>Home</title>
 </head>
 <body>
-    <main className="md (768px)">
+    <main className="sm (640px) flex bg-green-500">
     <div className="Btn-blue">
         <a href ='' name='EnterPortfolio'>Enter Portfolio</a>
     </div>
